c834333ec4d781d227d0718736079f3b81664331
丰硕 <fengshuochn@126.com>
2018-05-24 00:34:14 +0800

    feat: add test

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # http-detector
 a tool to detect http get/post data type
+
+# dectect
+- ajax
+- fetch
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('http-detector')
+const rawOpen = XMLHttpRequest.prototype.open
+const rawSend = XMLHttpRequest.prototype.close
+
+XMLHttpRequest.prototype.open = function (...args) {
+  rawOpen.apply(this, args)
+  this.method = args[0]
+  this.url = args[1]
+  this.async = args[2]
+}
+
+XMLHttpRequest.prototype.send = function (...args) {
+  rawSend.apply(this, args)
+}
+
+function isInURLs(url) {
+  
+}
+
+function verifyDataType(data) {
+
+}
